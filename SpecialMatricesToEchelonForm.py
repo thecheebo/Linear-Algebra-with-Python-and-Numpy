de39b36d9627aae2ceb5d2c3ff1b63c1f68ff45f
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-04-01 20:15:04 -0700

    Create SpecialMatricesToEchelonForm.py

---
+++ SpecialMatricesToEchelonForm.py
@@ -0,0 +1,132 @@
+# GRADED FUNCTION
+import numpy as np
+
+# Our function will go through the matrix replacing each row in order turning it into echelon form.
+# If at any point it fails because it can't put a 1 in the leading diagonal,
+# we will return the value True, otherwise, we will return False.
+# There is no need to edit this function.
+def isSingular(A) :
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+    try:
+        fixRowZero(B)
+        fixRowOne(B)
+        fixRowTwo(B)
+        fixRowThree(B)
+    except MatrixIsSingular:
+        return True
+    return False
+
+# This next line defines our error flag. For when things go wrong if the matrix is singular.
+# There is no need to edit this line.
+class MatrixIsSingular(Exception): pass
+
+# For Row Zero, all we require is the first element is equal to 1.
+# We'll divide the row by the value of A[0, 0].
+# This will get us in trouble though if A[0, 0] equals 0, so first we'll test for that,
+# and if this is true, we'll add one of the lower rows to the first one before the division.
+# We'll repeat the test going down each lower row until we can do the division.
+# There is no need to edit this function.
+def fixRowZero(A) :
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[1]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[2]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[3]
+    if A[0,0] == 0 :
+        raise MatrixIsSingular()
+    A[0] = A[0] / A[0,0]
+    return A
+
+# First we'll set the sub-diagonal elements to zero, i.e. A[1,0].
+# Next we want the diagonal element to be equal to one.
+# We'll divide the row by the value of A[1, 1].
+# Again, we need to test if this is zero.
+# If so, we'll add a lower row and repeat setting the sub-diagonal elements to zero.
+# There is no need to edit this function.
+def fixRowOne(A) :
+    A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[2]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[3]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        raise MatrixIsSingular()
+    A[1] = A[1] / A[1,1]
+    return A
+
+# This is the first function that you should complete.
+# Follow the instructions inside the function at each comment.
+def fixRowTwo(A) :
+    # Insert code below to set the sub-diagonal elements of row two to zero (there are two of them).
+    A[2] = A[2] - A[2,0] * A[0]
+    A[2] = A[2] - A[2,1] * A[1]
+    
+    # Next we'll test that the diagonal element is not zero.
+    if A[2,2] == 0 :
+        A[2] = A[2] + A[3]
+        A[2] = A[2] - A[2,0] * A[0]
+        A[2] = A[2] - A[2,1] * A[1]
+        
+        # Insert code below that adds a lower row to row 2.
+        
+        # Now repeat your code which sets the sub-diagonal elements to zero.
+        
+        
+    if A[2,2] == 0 :
+        raise MatrixIsSingular()
+    # Finally set the diagonal element to one by dividing the whole row by that element.
+    A[2] = A[2] / A[2,2]
+    
+    return A
+
+# You should also complete this function
+# Follow the instructions inside the function at each comment.
+def fixRowThree(A) :
+    # Insert code below to set the sub-diagonal elements of row three to zero.
+    
+    A[3] = A[3] - A[3,0] * A[0]
+    A[3] = A[3] - A[3,1] * A[1]
+    A[3] = A[3] - A[3,2] * A[2]
+    
+    # Complete the if statement to test if the diagonal element is zero.
+    if A[3,3] == 0 :
+        raise MatrixIsSingular()
+    # Transform the row to set the diagonal element to one.
+    A[3] = A[3] / A[3,3] 
+    
+    return A
+    
+  # First load Pyplot, a graph plotting library.
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+# This is the matrix of Bear's basis vectors.
+# (When you've done the exercise once, see what happns when you change Bear's basis.)
+bearBasis = np.array(
+    [[1,   -1],
+     [1.5, 2]])
+# This line uses your code to build a transformation matrix for us to use.
+T = build_reflection_matrix(bearBasis)
+
+# Bear is drawn as a set of polygons, the vertices of which are placed as a matrix list of column vectors.
+# We have three of these non-square matrix lists: bear_white_fur, bear_black_fur, and bear_face.
+# We'll make new lists of vertices by applying the T matrix you've calculated.
+reflected_bear_white_fur = T @ bear_white_fur
+reflected_bear_black_fur = T @ bear_black_fur
+reflected_bear_face = T @ bear_face
+
+# This next line runs a code to set up the graphics environment.
+ax = draw_mirror(bearBasis)
+
+# We'll first plot Bear, his white fur, his black fur, and his face.
+ax.fill(bear_white_fur[0], bear_white_fur[1], color=bear_white, zorder=1)
+ax.fill(bear_black_fur[0], bear_black_fur[1], color=bear_black, zorder=2)
+ax.plot(bear_face[0], bear_face[1], color=bear_white, zorder=3)
+
+# Next we'll plot Bear's reflection.
+ax.fill(reflected_bear_white_fur[0], reflected_bear_white_fur[1], color=bear_white, zorder=1)
+ax.fill(reflected_bear_black_fur[0], reflected_bear_black_fur[1], color=bear_black, zorder=2)
+ax.plot(reflected_bear_face[0], reflected_bear_face[1], color=bear_white, zorder=3);
